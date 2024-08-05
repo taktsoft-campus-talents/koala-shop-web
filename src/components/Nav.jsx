809a49a1bb762918744ed5f93aa83501cd64441f
maria-dsgn <151375515+maria-dsgn@users.noreply.github.com>
2024-08-05 13:04:55 +0200

    create nav component, add basic styling

---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import "./Nav.css";
+
+export function Nav() {
+  return (
+    <nav className="navbar">
+      <div className="logo">🐨</div>
+      <ul className="navbar-links">
+        <li>
+          <a href="#homepage">Homepage</a>
+        </li>
+        <li>
+          <a href="#products">Products</a>
+        </li>
+        <li>
+          <a href="#deals">Deals</a>
+        </li>
+        <li>
+          <a href="#aboutus">About Us</a>
+        </li>
+        <li>
+          <a href="#cart">Cart 🛒</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
 
 export function RootLayout() {
   return (
     <>
+      <Nav />
       <h1>The Koala Shop</h1>
       <Outlet />
     </>
