b3e792a3db22b12d5f3780321f2b5fb19771b2e6
maria-dsgn <151375515+maria-dsgn@users.noreply.github.com>
2024-08-06 17:13:26 +0200

    create util function and error logic to product details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,28 +1,42 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-//import "./ProductDetails.css";
+import { formatPrice } from "../utils/utils";
+import "./ProductDetails.css";
+
+const URL = import.meta.env.VITE_API_URL;
 
 export function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-  const URL = "http://localhost:3000"; // replace with the actual api URL later
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProductDetails = async () => {
       try {
         const response = await fetch(`${URL}/products/${id}`);
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+
         const data = await response.json();
         setProduct(data);
       } catch (error) {
         console.error("Error fetching product details:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchProductDetails();
   }, [id]);
 
+  if (loading) {
+    return <div>Loading...</div>; // Loading state
+  }
+
   if (!product) {
-    return <div>Loading...</div>;
+    return <div className="error-message">Product not found.</div>;
   }
 
   return (
@@ -33,7 +47,9 @@ export function ProductDetails() {
         className="product-details-image"
       />
       <h1 className="product-details-title">{product.title}</h1>
-      <p className="product-details-price">Price: €{product.price}</p>
+      <p className="product-details-price">
+        Price: {formatPrice(product.price)}
+      </p>
       <p className="product-details-description">{product.description}</p>
       <div
         className={
