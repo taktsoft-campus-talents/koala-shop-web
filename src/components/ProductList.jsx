165623f6eb7c1422ab41c60ce9b46cedf857ba93
Ralf Siewert <ralf@actyvy.st>
2024-08-06 10:51:22 +0200

    change product data format to match API format

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,34 +2,42 @@ import { useEffect, useState } from "react";
 import { ProductCard } from "./ProductCard";
 import "./ProductList.css";
 
+const URL = import.meta.env.VITE_API_URL; // get url from Henning
+
+// const dummyData = [
+//   {
+//     title: "Kühlschrank",
+//     category: "Technik",
+//     description: "hält sehr kühl. wichtig für Eis",
+//     img: "https://m.media-amazon.com/images/I/81If+KcnIkL._AC_UF894,1000_QL80_.jpg",
+//     price: 350.0,
+//     leftInStock: 4,
+//   },
+//   {
+//     title: "Schuhe",
+//     category: "Mode",
+//     description: "für an die Füsse. halten warm",
+//     img: "https://m.media-amazon.com/images/I/81If+KcnIkL._AC_UF894,1000_QL80_.jpg",
+//     price: 120.0,
+//     leftInStock: 16,
+//   },
+// ];
+
 export function ProductList() {
-  const [products, setProducts] = useState([
-    {
-      title: "Kühlschrank",
-      category: "Technik",
-      desc: "hält sehr kühl. wichtig für Eis",
-      img: "https://m.media-amazon.com/images/I/81If+KcnIkL._AC_UF894,1000_QL80_.jpg",
-      price: 350.0,
-      stock: 4,
-    },
-    {
-      title: "Schuhe",
-      category: "Mode",
-      desc: "für an die Füsse. halten warm",
-      img: "https://m.media-amazon.com/images/I/81If+KcnIkL._AC_UF894,1000_QL80_.jpg",
-      price: 120.0,
-      stock: 16,
-    },
-  ]);
+  const [products, setProducts] = useState([]);
 
   //fetch data from server api
-  const URL = "http://...."; // get url from Henning
+
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await fetch(`${URL}/products`);
         const data = await response.json();
-        setProducts(data);
+        if (response.ok) {
+          setProducts(data);
+        } else {
+          console.log(data);
+        }
       } catch (error) {
         console.error("Error fetching products:", error);
       }
