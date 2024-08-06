3abb874055a180fdcbeb2598c4d1c34024ac1251
Gladys Sobrido Sambade <gladys@MacBook-Pro-von-Gladys.local>
2024-08-06 15:23:35 +0200

    input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export function Home() {
-  return <h2>Home</h2>;
+  const [name, setName] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <>
+      <h2>Home</h2>
+      <form action="" onSubmit={(event) => handleSubmit(event)}>
+        <input onChange={(event) => setName(event.target.value)} />
+        <button type="submit">Log in</button>
+        Your name: {name}
+      </form>
+    </>
+  );
 }
