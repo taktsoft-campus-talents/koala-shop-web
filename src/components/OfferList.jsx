ddd8a6e1343ef0c5a0793fc874b217b2adf38a07
Oleksii Butrin <bootreen@gmail.com>
2024-08-07 14:38:08 +0200

    Normal fetch version (untested, as we require offers API implementation)

--- src/components/OfferList.jsx
+++ src/components/OfferList.jsx
@@ -1,36 +1,36 @@
 import "./OfferList.css";
 import { useEffect, useState } from "react";
-// const URL = import.meta.env.VITE_API_URL;
+const URL = import.meta.env.VITE_API_URL;
 
 export const OfferList = () => {
   const [offers, setOffers] = useState([]);
 
   useEffect(() => {
     const fetchOffers = async () => {
-      // try {
-      //   const response = await fetch(`${URL}/offers`);
-      //   const data = await response.json();
-      //   if (response.ok) {
-      //     setOffers(data);
-      //   } else {
-      //     console.log(data);
-      //   }
-      // } catch (error) {
-      //   console.error("Error fetching products:", error);
-      // }
+      try {
+        const response = await fetch(`${URL}/offers`);
+        const data = await response.json();
+        if (response.ok) {
+          setOffers(data);
+        } else {
+          console.log(data);
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
 
       // Dummy data
-      setOffers([
-        { productId: 1, productTitle: "Awesome product" },
-        { productId: 2, productTitle: "Awesome product XS" },
-        { productId: 3, productTitle: "Awesome product XXL" },
-      ]);
+      // setOffers([
+      //   { productId: 1, productTitle: "Awesome product" },
+      //   { productId: 2, productTitle: "Awesome product XS" },
+      //   { productId: 3, productTitle: "Awesome product XXL" },
+      // ]);
     };
 
     fetchOffers();
   }, []);
 
-  // [ { productId, productTitle }, ...]
+  // Payload format: [ { productId, productTitle }, ...]
   return (
     <div className="offer-list-container">
       <h3>Special offers:</h3>
