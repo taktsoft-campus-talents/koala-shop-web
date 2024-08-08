5928e983f86ac4982230d5a0b3f6ae3fd8694980
Gladys Sobrido Sambade <gladys@MacBook-Pro-von-Gladys.local>
2024-08-08 14:06:02 +0200

    first steps

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,25 @@
 import { PromoCard } from "../components/PromoCard";
 import "./Home.css";
+import QRCode from "qrcode";
 
 export function Home() {
+  const generateQR = async (text) => {
+    try {
+      console.log(await QRCode.toDataURL(text));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  console.log(generateQR);
   return (
     <>
       <div className="homepage">
         <div className="homepage-content">
           <h2>Home</h2>
+          <p>Scan this code to get a exclusive offer:</p>
+          <img src="out.png" />
+          <canvas id="canvas"></canvas>
+
           <PromoCard
             offer={{
               id: 1,
